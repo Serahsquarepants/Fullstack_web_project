6a9d6810b445a2b7d79ad20b94e242878f46e975
Dimitar Iliev <dimitar@MacBook-Pro-de-Dimitar.local>
2023-03-02 20:07:51 +0100

    ?

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,23 +9,24 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import dotenv
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+SECRET_KEY = os.environ['SECRET_KEY']    
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['127.0.0.1', 'https://serah.pythonanywhere.com']
 
+DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
